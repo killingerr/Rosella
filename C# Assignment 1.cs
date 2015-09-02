453ec42cdd80512ac3ad4fd2f15ec83aee9cd5f0
killingerr <rosella.tony@gmail.com>
2015-09-01 21:18:45 -0400

    Update

---
+++ C# Assignment 1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int num2;
+            
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+            int difference = num1 - num2;
+            int product = num1 * num2;
+            int quotient = num1 / num2;
+
+
+            Console.WriteLine("Sum is:{0}\nDifference is:{1}\nProduct is:{2}\nQuotient is:{3}", sum, difference, product, quotient);
+            Console.Read();
+
+
+        }
+    }
+}
