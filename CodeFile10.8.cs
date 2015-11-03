6df810eb28dcf5585b33fd7788666e3be9bdb7a1
killingerr <rosella.tony@gmail.com>
2015-11-03 15:29:05 -0500

    Assignment 10.8

---
+++ CodeFile10.8.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class IntegerSet
+{
+    bool[] array1 = new bool[101];  // Array with 100 elements
+    public IntegerSet()  // Constructor
+    
+    {
+        for (int i = 0; i < array1.Length; i++ )  // Initializing all elements in array to false
+        {
+            array1[i] = false;
+        }
+
+    }
+    
+    public void InsertElement(int i)  // Inserts true into element
+    {
+        array1[i] = true;
+
+    }
+
+    public void DeleteElement(int i)  // Deletes true in element to make it false
+    {
+        array1[i] = false;
+
+
+    }
+
+    public override string ToString()  // ToString method used for dislaying dat within the array
+    {
+        string k = " ";
+
+        for(int i = 0; i <= 100; i++)
+        {
+            if(this.array1[i] == true)
+            {
+                k += i + " ";
+            }
+            else
+            {
+                k += "___";
+            }
+        
+        }
+        return k;
+    }
+    
+    public bool IsEqualTo(IntegerSet k)  // Method comparing two objects data within an array
+    {
+        for(int i = 0; i < array1.Length; i++)
+        {
+            if (k.array1[i] != array1[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+       
+    
+}
---
+++ Program10.8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10._8
+{
+    class Program
+    {
+
+    public class IntegerSet2 : IntegerSet        // Derived lass from the parent class Integer set
+    {
+         
+
+    
+        static void Main(string[] args)
+        {
+            IntegerSet2 iSet = new IntegerSet2();  // Initialized object using derived class
+            IntegerSet2 iSet2 = new IntegerSet2();  // Initialized object using derived class
+
+             iSet.InsertElement(56);  // Object of derived with data inserted from method
+             iSet2.InsertElement(24); // Object of derived with data inserted from method
+             
+             Console.WriteLine(iSet.ToString());  // Display current data of object
+             Console.WriteLine(iSet2.ToString()); // Display current data of object
+            
+             iSet.DeleteElement(56);  // Object of derived with data deleted using method
+             iSet2.DeleteElement(24);  // Object of derived with data deleted using method
+
+             Console.WriteLine(iSet.ToString()); // Display current data of object
+             Console.WriteLine(iSet2.ToString()); // Display current data of object
+             
+             iSet.IsEqualTo(iSet2);  // Method comparing iSet object to iSet2 object
+             
+        }
+    }
+    }
+}
