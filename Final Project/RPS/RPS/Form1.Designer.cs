beabe06fde4f590adcfd4f09785746f298f059f2
killingerr <rosella.tony@gmail.com>
2015-12-08 18:36:25 -0500

    Changes

--- Final Project/RPS/RPS/Form1.Designer.cs
+++ Final Project/RPS/RPS/Form1.Designer.cs
@@ -51,9 +51,10 @@
             // RockButton
             // 
             this.RockButton.Image = ((System.Drawing.Image)(resources.GetObject("RockButton.Image")));
-            this.RockButton.Location = new System.Drawing.Point(246, 348);
+            this.RockButton.Location = new System.Drawing.Point(328, 428);
+            this.RockButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RockButton.Name = "RockButton";
-            this.RockButton.Size = new System.Drawing.Size(100, 100);
+            this.RockButton.Size = new System.Drawing.Size(133, 123);
             this.RockButton.TabIndex = 0;
             this.RockButton.UseVisualStyleBackColor = true;
             this.RockButton.Click += new System.EventHandler(this.RockButton_Click);
@@ -61,9 +62,10 @@
             // PaperButton
             // 
             this.PaperButton.Image = ((System.Drawing.Image)(resources.GetObject("PaperButton.Image")));
-            this.PaperButton.Location = new System.Drawing.Point(383, 348);
+            this.PaperButton.Location = new System.Drawing.Point(511, 428);
+            this.PaperButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PaperButton.Name = "PaperButton";
-            this.PaperButton.Size = new System.Drawing.Size(100, 100);
+            this.PaperButton.Size = new System.Drawing.Size(133, 123);
             this.PaperButton.TabIndex = 1;
             this.PaperButton.UseVisualStyleBackColor = true;
             this.PaperButton.Click += new System.EventHandler(this.PaperButton_Click);
@@ -71,52 +73,57 @@
             // ScissorsButton
             // 
             this.ScissorsButton.Image = ((System.Drawing.Image)(resources.GetObject("ScissorsButton.Image")));
-            this.ScissorsButton.Location = new System.Drawing.Point(523, 348);
+            this.ScissorsButton.Location = new System.Drawing.Point(697, 428);
+            this.ScissorsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ScissorsButton.Name = "ScissorsButton";
-            this.ScissorsButton.Size = new System.Drawing.Size(100, 100);
+            this.ScissorsButton.Size = new System.Drawing.Size(133, 123);
             this.ScissorsButton.TabIndex = 2;
             this.ScissorsButton.UseVisualStyleBackColor = true;
             this.ScissorsButton.Click += new System.EventHandler(this.ScissorsButton_Click);
             // 
             // StartButton
             // 
-            this.StartButton.Font = new System.Drawing.Font("SonicCutThru Hv BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartButton.Location = new System.Drawing.Point(271, 526);
+            this.StartButton.Font = new System.Drawing.Font("Magneto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(361, 647);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(75, 65);
+            this.StartButton.Size = new System.Drawing.Size(100, 80);
             this.StartButton.TabIndex = 3;
-            this.StartButton.Text = "start";
+            this.StartButton.Text = "Start";
             this.StartButton.UseVisualStyleBackColor = true;
             this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
             // ResetButton
             // 
-            this.ResetButton.Font = new System.Drawing.Font("SonicCutThru Hv BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResetButton.Location = new System.Drawing.Point(396, 526);
+            this.ResetButton.Font = new System.Drawing.Font("Magneto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton.Location = new System.Drawing.Point(528, 647);
+            this.ResetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ResetButton.Name = "ResetButton";
-            this.ResetButton.Size = new System.Drawing.Size(75, 65);
+            this.ResetButton.Size = new System.Drawing.Size(100, 80);
             this.ResetButton.TabIndex = 4;
-            this.ResetButton.Text = "reset";
+            this.ResetButton.Text = "Reset";
             this.ResetButton.UseVisualStyleBackColor = true;
             this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
             // 
             // QuitButton
             // 
-            this.QuitButton.Font = new System.Drawing.Font("SonicCutThru Hv BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.QuitButton.Location = new System.Drawing.Point(523, 526);
+            this.QuitButton.Font = new System.Drawing.Font("Magneto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuitButton.Location = new System.Drawing.Point(697, 647);
+            this.QuitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.QuitButton.Name = "QuitButton";
-            this.QuitButton.Size = new System.Drawing.Size(75, 65);
+            this.QuitButton.Size = new System.Drawing.Size(100, 80);
             this.QuitButton.TabIndex = 5;
-            this.QuitButton.Text = "quit";
+            this.QuitButton.Text = "Quit";
             this.QuitButton.UseVisualStyleBackColor = true;
             this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.pictureBox1.Location = new System.Drawing.Point(246, 40);
+            this.pictureBox1.Location = new System.Drawing.Point(328, 49);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(377, 295);
+            this.pictureBox1.Size = new System.Drawing.Size(503, 363);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -124,77 +131,85 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("SonicCutThru Hv BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(44, 122);
+            this.label1.Font = new System.Drawing.Font("Magneto", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 34);
+            this.label1.Size = new System.Drawing.Size(244, 41);
             this.label1.TabIndex = 8;
             this.label1.Text = "Player Wins";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("SonicCutThru Hv BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(629, 122);
+            this.label2.Font = new System.Drawing.Font("Magneto", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(839, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 34);
+            this.label2.Size = new System.Drawing.Size(202, 41);
             this.label2.TabIndex = 9;
             this.label2.Text = "A.I. Wins";
             // 
             // playerLabel
             // 
             this.playerLabel.AutoSize = true;
-            this.playerLabel.Font = new System.Drawing.Font("SonicCutThru Hv BT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.playerLabel.Location = new System.Drawing.Point(147, 177);
+            this.playerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerLabel.Location = new System.Drawing.Point(196, 218);
+            this.playerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.playerLabel.Name = "playerLabel";
-            this.playerLabel.Size = new System.Drawing.Size(0, 14);
+            this.playerLabel.Size = new System.Drawing.Size(0, 17);
             this.playerLabel.TabIndex = 10;
             // 
             // npcLabel
             // 
             this.npcLabel.AutoSize = true;
-            this.npcLabel.Font = new System.Drawing.Font("SonicCutThru Hv BT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.npcLabel.Location = new System.Drawing.Point(683, 177);
+            this.npcLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.npcLabel.Location = new System.Drawing.Point(911, 218);
+            this.npcLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.npcLabel.Name = "npcLabel";
-            this.npcLabel.Size = new System.Drawing.Size(0, 14);
+            this.npcLabel.Size = new System.Drawing.Size(0, 17);
             this.npcLabel.TabIndex = 11;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("SonicCutThru Hv BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(629, 9);
+            this.label5.Font = new System.Drawing.Font("Magneto", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(839, 11);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(151, 25);
+            this.label5.Size = new System.Drawing.Size(181, 30);
             this.label5.TabIndex = 12;
             this.label5.Text = "Rounds Won";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("SonicCutThru Hv BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(91, 9);
+            this.label6.Font = new System.Drawing.Font("Magneto", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(121, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(151, 25);
+            this.label6.Size = new System.Drawing.Size(181, 30);
             this.label6.TabIndex = 13;
             this.label6.Text = "Rounds Won";
             // 
             // roundsWonN
             // 
             this.roundsWonN.AutoSize = true;
-            this.roundsWonN.Font = new System.Drawing.Font("SonicCutThru Hv BT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.roundsWonN.Location = new System.Drawing.Point(683, 49);
+            this.roundsWonN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundsWonN.Location = new System.Drawing.Point(911, 60);
+            this.roundsWonN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.roundsWonN.Name = "roundsWonN";
-            this.roundsWonN.Size = new System.Drawing.Size(0, 14);
+            this.roundsWonN.Size = new System.Drawing.Size(0, 17);
             this.roundsWonN.TabIndex = 14;
             // 
             // roundsWonP
             // 
             this.roundsWonP.AutoSize = true;
-            this.roundsWonP.Font = new System.Drawing.Font("SonicCutThru Hv BT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.roundsWonP.Location = new System.Drawing.Point(147, 49);
+            this.roundsWonP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundsWonP.Location = new System.Drawing.Point(196, 60);
+            this.roundsWonP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.roundsWonP.Name = "roundsWonP";
-            this.roundsWonP.Size = new System.Drawing.Size(0, 14);
+            this.roundsWonP.Size = new System.Drawing.Size(0, 17);
             this.roundsWonP.TabIndex = 15;
             // 
             // comboBox1
@@ -205,17 +220,18 @@
             "First to 5",
             "First to 7",
             "First to 9"});
-            this.comboBox1.Location = new System.Drawing.Point(28, 504);
+            this.comboBox1.Location = new System.Drawing.Point(37, 620);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 16;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 631);
+            this.ClientSize = new System.Drawing.Size(1183, 777);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.roundsWonP);
             this.Controls.Add(this.roundsWonN);
@@ -233,6 +249,7 @@
             this.Controls.Add(this.PaperButton);
             this.Controls.Add(this.RockButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
